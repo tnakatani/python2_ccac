87abf3358ab3491573a840c2cc29cbb18bc4ae55
Taichi Nakatani <tnakatani@gmail.com>
2020-01-29 19:54:01 -0500

    init commit

---
+++ wk_1/icon_maker.py
@@ -0,0 +1,13 @@
+"""
+Create a program in python that uses looping and basic data structures to display customized 10x10 student icons in various scales and orientations.
+
+Create a data structure (list, tuple, dictionary) that can represent the sequence of on and off cells in each icon inside your python program
+
+Read your custom data structure to display a visual representation of your icon using a single symbol, such as an asterisk or an at-sign.
+
+Implement one or more transformations on a given icon, starting with scaling, such that each "cell" is represnted by not just one character, but a square of characters of a given size. Consider other transformations such as inverting on and off cells, or rotating by a specified amount of degrees
+
+Review your code and break it up into as many sensible functions as possible, allowing for code re-use. Comment each function such that its purpose is clear to other users.
+
+Post your code on your github account
+"""
