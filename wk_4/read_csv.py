d4d9d01cfb5234930082389d31d60e362f60261a
tnakatani <tnakatani@gmail.com>
2020-02-20 22:39:40 -0500

    commit return_null_table()

--- wk_4/read_csv.py
+++ wk_4/read_csv.py
@@ -1,72 +1,91 @@
-"""
-id,name,task_description,area,budgeted_amount,status,asset_id,asset_type,fiscal_year,start_date,inactive,neighborhood,council_district,ward,tract,public_works_division,pli_division,police_zone,fire_zone,latitude,longitude
-"""
-
 import json
 import csv
+import pandas as pd
 
-# def reject_empty_field_rows(dataset):
+def value_is_empty(row, item):
+    return row[item] == ''
 
+def return_null_table(dataset):
+    """
+    Return a frequency table of null values for each dataset key
+    """
+    null_table={}
+    header = dataset.fieldnames
+    # skip header
+    next(dataset)
+    for row in dataset:
+        for item in header:
+            #if value_is_empty(row,item):
+            if row[item] == '':
+                if item not in null_table:
+                    null_table[item] = 1
+                else:
+                    null_table[item] += 1
+            else:
+                if item not in null_table:
+                    null_table[item] = 0
+    return null_table
 
-def read_dataset(dataset, criteria):
-    with open(dataset,'r') as f:
-        reader = csv.DictReader(f)
-        print(reader)
-        for i, row in enumerate(reader):
-            if i <= 5:
-                print(row)
-        print(criteria)
-        blank_table={}
-        criterias = ['fiscal_year', 'area']
-        """
-        if any cell in this row is empty, move to the new row
-        """
-        #for i, row in enumerate(reader):
-            #if i <= 100:
-                # for every criteria in criterias:
-                    # if row[criteria] = ''
-                        # blank_table[criteria] += 1
-                        # else:
-                        # blank_table[criteria] + 1
-                #if row['fiscal_year'] = ''
-                #    if 'fiscal_year' in table:
-                #        blank_table['fiscal_year'] += 1
-                #    else:
-                #        blank_table['fiscal_year'] = 1
-                #    continue
-                #elif: row['start_date'] = ''
-                #    if 'fiscal_year' in table:
-                #        blank_table['fiscal_year'] += 1
-                #    else:
-                #        blank_table['fiscal_year'] = 1
-                #elif: row['area'] = ''
-                #    if 'fiscal_year' in table:
-                #        blank_table['fiscal_year'] += 1
-                #    else:
-                #        blank_table['fiscal_year'] = 1
-                #elif: row['asset_type'] = ''
-                #    if 'fiscal_year' in table:
-                #        blank_table['fiscal_year'] += 1
-                #    else:
-                #        blank_table['fiscal_year'] = 1
-                #elif: row['planning_status'] = ''
-                #    if 'fiscal_year' in table:
-                #        blank_table['fiscal_year'] += 1
-                #    else:
-                #        blank_table['fiscal_year'] = 1
+def return_filtered_table(dataset, specification):
+    """
+    Read specification from specification file, and return only rows that meet
+    its specifications
 
-   # return table
+    - A blank value in any specified query for a column/field will disqualify
+      that record from inclusion in the results
+    - Empty string: do not limit results by this specification at all
+    """
+    match_table = {}
+    header = dataset.fieldnames
+    for row in dataset:
+        for item in header:
+            if row[item] == '':
+                continue
+        if row['status'] == specification['status'][0] and \
+            row['neighborhood'] == specification['neighborhood'][0]:
+            print(row)
 
 
-
-def read_criteria(criteria):
-    with open(criteria) as f:
-        reader = json.load(f)
-    return reader
+    # try to loop through each list of specs and use each as a combination
+    # of conditions (ie. status - completed and neighborhood - shadyside,
+    # status - completed and neighborhood - squirrel hill
+    for spec in specification:
+        print(specification[spec])
+       
+        # this is more or less an "or" condition for any specification
+        # for item in specification:
+        #     if row[item] == specification[item][0]:
+        #         print('result',row)
+        
 
 if __name__ == '__main__':
-    dataset_path = './data/pgh_capital_projects.csv'
-    json_path = 'search_criteria.json'
+    """ Vanilla Python """
+    print('Vanilla Python')
+
+    with open('./data/pgh_capital_projects.csv') as d, \
+        open('search_criteria.json') as c:
+        dataset = csv.DictReader(d)
+        specification = json.load(c)
+
+        # Return null table
+        print("\nReturn null table:")
+        # print(return_null_table(dataset))
+
+        # Return rows that meets the imported specification
+        print("\nReturn rows that meets the imported specification")
+        return_filtered_table(dataset, specification)
+
+
+    """ Pandas version"""
+    """
+    print('\nPandas version:')
+    df = pd.read_csv('data/pgh_capital_projects.csv')
     
-    json_criteria = read_criteria(json_path)
-    read_dataset(dataset_path, json_criteria)
+    # return_null_table
+    print("\nReturn null table:")
+    print(df.isnull().sum())
+    """
+
+# mock json
+# {"fiscal_year": [2018], "start_date": [""], "area": [""], "asset_type": [""],  "status": [""]}
+
