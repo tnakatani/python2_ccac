8561444a52410ac3a4eb73f434a9087bc51db399
tnakatani <tnakatani@gmail.com>
2020-02-23 21:54:31 -0500

    add query filtering capability; TODO: Abstract it to avoid specifying keys

--- wk_4/README.md
+++ wk_4/README.md
@@ -1,3 +1,4 @@
-# JSON
+Notes while writing script
 
+1. Why can't I write with statement that runs multiple scripts inside it?  Second function that does a for loop function is returning None.
 
--- wk_4/read_csv.py
+++ wk_4/read_csv.py
@@ -1,32 +1,72 @@
 import json
 import csv
-import pandas as pd
+import logging as log
+#import pandas as pd
 
-def value_is_empty(row, item):
-    return row[item] == ''
+############################################################
+# Helper Functions
+############################################################
 
-def return_null_table(dataset):
+def lowercase_list(l):
+    return [i.lower() for i in l]
+
+def log_specifications(specification):
+    print('querying the following specifications:\n'.upper())
+    for spec, value in specification.items():
+        msg = f'{spec.upper()} : {str(value)}'
+        print(msg.upper())
+
+def log_count(match):
+    msg = (f'\nquery matched {count} rows:\n')
+    print(msg.upper())
+
+def log_status(message):
+    div = "#"*80
+    msg = (f'\n{div}\n{message}\n{div}\n')
+    print(msg.upper())
+
+############################################################
+# Analysis Logic
+############################################################
+
+def create_null_table(dataset):
     """
     Return a frequency table of null values for each dataset key
     """
     null_table={}
     header = dataset.fieldnames
-    # skip header
-    next(dataset)
+
+    next(dataset) # skip header
     for row in dataset:
         for item in header:
-            #if value_is_empty(row,item):
-            if row[item] == '':
-                if item not in null_table:
-                    null_table[item] = 1
-                else:
+            if not row[item]: # using 'implicit booleanness'
+                if item in null_table:
                     null_table[item] += 1
+                else:
+                    null_table[item] = 1
             else:
                 if item not in null_table:
                     null_table[item] = 0
     return null_table
 
-def return_filtered_table(dataset, specification):
+def fields_exist_in_row(row, specification):
+    """
+    Returns boolean of whether row contains field that matches specifications
+    for each key
+
+    ToDo: How to iterate this without specifying key values?
+    """
+    statuses = lowercase_list(specification['status'])
+    neighborhoods = lowercase_list(specification['neighborhood'])
+    fiscal_years = str(specification['fiscal_year'])
+    areas = lowercase_list(specification['area'])
+    if row['status'].lower() in statuses \
+            and row['neighborhood'].lower() in neighborhoods \
+            and row['fiscal_year'] in fiscal_years \
+            and row['area'].lower() in areas:
+        return True
+
+def query_dataset(dataset, specification):
     """
     Read specification from specification file, and return only rows that meet
     its specifications
@@ -35,45 +75,43 @@ def return_filtered_table(dataset, specification):
       that record from inclusion in the results
     - Empty string: do not limit results by this specification at all
     """
-    match_table = {}
-    header = dataset.fieldnames
+    matched_rows = []
+    matched_count = 0
+
+    next(dataset) # skip header
+    # loop through each row
     for row in dataset:
-        for item in header:
-            if row[item] == '':
+        # loop through each spec
+        for spec in specification:
+            # skip any values where there is an empty field
+            if row[spec] == '':
                 continue
-        if row['status'] == specification['status'][0] and \
-            row['neighborhood'] == specification['neighborhood'][0]:
-            print(row)
-
-
-    # try to loop through each list of specs and use each as a combination
-    # of conditions (ie. status - completed and neighborhood - shadyside,
-    # status - completed and neighborhood - squirrel hill
-    for spec in specification:
-        print(specification[spec])
-       
-        # this is more or less an "or" condition for any specification
-        # for item in specification:
-        #     if row[item] == specification[item][0]:
-        #         print('result',row)
-        
+            elif fields_exist_in_row(row, specification):
+                matched_count += 1
+                matched_rows.append(row)
+    return matched_rows, matched_count
 
 if __name__ == '__main__':
-    """ Vanilla Python """
-    print('Vanilla Python')
+    log_status('return frequency table of null values for each key')
 
     with open('./data/pgh_capital_projects.csv') as d, \
         open('search_criteria.json') as c:
         dataset = csv.DictReader(d)
         specification = json.load(c)
 
-        # Return null table
-        print("\nReturn null table:")
-        # print(return_null_table(dataset))
+        print(create_null_table(dataset))
 
-        # Return rows that meets the imported specification
-        print("\nReturn rows that meets the imported specification")
-        return_filtered_table(dataset, specification)
+    log_status('return rows that meets the imported specifications')
+    with open('./data/pgh_capital_projects.csv') as d, \
+        open('search_criteria.json') as c:
+        dataset = csv.DictReader(d)
+        specification = json.load(c)
+
+        rows, count = query_dataset(dataset, specification)
+        log_specifications(specification)
+        log_count(count)
+        for row in rows:
+            print(row)
 
 
     """ Pandas version"""
