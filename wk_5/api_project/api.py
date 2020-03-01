0f85d13abecfb53560699dce6bd5984e3ac11c95
tnakatani <tnakatani@gmail.com>
2020-03-01 16:28:29 -0500

    'make script write to file'

--- wk_5/api_project/api.py
+++ wk_5/api_project/api.py
@@ -1,8 +1,6 @@
 import requests
 import json
-
-key = 'kHMaTxVJxeCDNOuOJdqK'
-secret = 'ykrkhBryxPLbtCtVPcMKzovBOdHNPwkz'
+import csv
 
 # artist = 'nirvana'
 # url = f'https://api.discogs.com/database/search?q={artist}&key={key}&secret={secret}&page=1&per_page=100'
@@ -16,17 +14,43 @@ secret = 'ykrkhBryxPLbtCtVPcMKzovBOdHNPwkz'
 #     for r in results:
 #         print(r['title'])
 
-username = 'kittertea'
-status = 'for sale'
-sort = 'price'
-sort_order = 'descend'
-url_user = f'https://api.discogs.com/users/{username}/inventory?status={status}&sort={sort}&page=1&per_page=100&key={key}&secret={secret}'
 req = requests.get(url_user)
 
-if int(req.status_code) == 200:
-    apiDict = json.loads(req.text)
-    print(apiDict.keys())
-    print(apiDict['listings'][0]['price']['currency'])
+def response_is_200():
+    int(req.status_code) == 200
+
+with open('seller_listings.csv','w') as f, open('sample_listing.json','w') as j:
+    key = 'kHMaTxVJxeCDNOuOJdqK'
+    secret = 'ykrkhBryxPLbtCtVPcMKzovBOdHNPwkz'
+    username = 'kittertea'
+    status = 'for sale'
+    sort = 'price'
+    sort_order = 'desc'
+    url_user = f'https://api.discogs.com/users/{username}/inventory?status={status}&sort={sort}&sort_order={sort_order}&page=1&per_page=100&key={key}&secret={secret}'
+    headers = ['listings',
+               'title',
+               'price',
+               'currency',
+               'url']
+    writer = csv.writer(f)
+    writer.writerow(headers)
+
+    try:
+        requests.get(url_user)
+        apidict = json.loads(req.text)
+
+        json.dump(apidict['listings'][0]['release'], j, indent=4, ensure_ascii=False)
+
+        for item in apidict['listings']:
+            artist = item['release']['artist']
+            title = item['release']['title']
+            price = item['price']['value']
+            currency = item['price']['currency']
+            url = item['uri']
+            writer.writerow([artist,title,price,currency,url])
+    except requests.exceptions.RequestException as e:
+        print(e)
+        sys.exit(1)
 
 
 
