243aee77ad4432a17b5bd5bcd419c91ef28f3979
tnakatani <tnakatani@gmail.com>
2020-03-11 00:30:19 -0400

    edit variables

--- wk_6/account_challenge/solution/account.py
+++ wk_6/account_challenge/solution/account.py
@@ -35,7 +35,7 @@ class Account:
         self.balance -= amount
 
 # Create an instance of a class
-james_account = Account('James Gosling', 50000)
+james_account = Account('James Gosling', 20000000)
 
 # Let's deposit some money in there
 james_account.deposit(500)
@@ -46,8 +46,8 @@ james_account.withdraw(1000)
 # Let's see Eric's balance
 print(james_account.balance)
 
-# Let's see Eric's user_id
-print(james_account.__user_id)
+# Let's see Eric's user_id (this will throw an error)
+# print(james_account.__user_id)
 
 
 # Inheritance example: Create a child class from Account
